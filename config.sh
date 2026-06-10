50b76a56fe7a5684f30ba6b51bda6e39284f3988
Yuugentsi <yuugentsi@users.noreply.github.com>
2026-06-10 17:46:19 -0300

    update

--- config.sh
+++ config.sh
@@ -8,5 +8,7 @@ if ! grep -q "grimler.se" "$PREFIX/etc/apt/sources.list" 2>/dev/null; then
     echo "deb https://grimler.se/termux-packages-24 stable main" > "$PREFIX/etc/apt/sources.list"
 fi
 
+touch "$HOME/.hushlogin"
+
 pkg update -y
 pkg upgrade -y
